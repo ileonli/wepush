42e24d4fcd5caca46b118184725a2b4d46084a77
Leon Li <imleonli@outlook.com>
2025-02-27 11:05:26 +0800

    init: project

---
+++ src/main/java/win/onebyte/wepush/WePushApplication.java
@@ -0,0 +1,13 @@
+package win.onebyte.wepush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WePushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WePushApplication.class, args);
+    }
+
+}
